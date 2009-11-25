72f93111ad05482f33baf2537709cfb5d0d675b0
Pranab <pkghosh99@gmail.com>
2009-11-24 23:41:09 -0800

    view, migration template

--- generators/crm_email/templates/migration.rb
+++ generators/crm_email/templates/migration.rb
@@ -1,20 +1,21 @@
 class CreateEmails < ActiveRecord::Migration
   def self.up
     create_table :emails, :force => true do |table|
-      table.string   :uuid, :null => false          
+      table.string   :msgid, :null => false          
       table.string   :from, :null => false          
       table.string   :to, :null => false          
       table.string   :cc                             
       table.string   :bcc          
       table.string   :subject          
       table.text     :body
+      table.string   :ref_msgid          
       table.datetime :received_at    
       table.references  :user, :null => false      
       table.references  :comment     
       table.timestamps
     end
     
-    create_table :contacts_emails, :force => true do |table|
+    create_table :contacts_emails, :id => false, :force => true do |table|
       table.references  :contact, :null => false     
       table.references  :email, :null => false     
       table.timestamps
--- init.rb
+++ init.rb
@@ -1,4 +1,19 @@
-# Include hook code here
+# Email plugin for Fat Free CRM
+# Copyright (C) 2009-2010 by Pranab Ghosh
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+# 
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#------------------------------------------------------------------------------
 require "fat_free_crm"
 
 FatFreeCRM::Plugin.register(:crm_email, initializer) do
@@ -8,3 +23,5 @@ FatFreeCRM::Plugin.register(:crm_email, initializer) do
    description "IMAP client for searching and fetching emails"
   dependencies :haml, :simple_column_search
 end
+
+#require 'crm_email'
